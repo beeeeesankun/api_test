c18a7ff71a2acb07d22c40e998e895a1344c56e1
akihiro-f <akihirobluesharks@gmail.com>
2021-10-26 22:11:50 +0900

    innit

---
+++ src/components/axios.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>axios</div>
+</template>
+
+<script>
+export default {
+  name: axios,
+  data: {
+    users: [],
+  },
+  mounted: function () {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => (this.users = response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import axios from "axios"; // 追加します。
+import VueAxios from "vue-axios"; // 追加します。
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios); // 追加します。
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
